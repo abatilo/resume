cb79797d529b518cf50ce1700f793ec7a0b36e77
Aaron <AaronBatilo@gmail.com>
2020-05-25 09:50:41 -0600

    Use IngressRoute CRD

--- index.ts
+++ index.ts
@@ -4,8 +4,9 @@ import * as pulumi from "@pulumi/pulumi";
 
 const clusterStackRef = new pulumi.StackReference("prod");
 const kubeconfig = clusterStackRef.getOutput("kubeconfig");
+
 const k8sProvider = new k8s.Provider("prod", {
-  kubeconfig,
+  kubeconfig: kubeconfig.apply(JSON.stringify),
 });
 
 const appName = "resume";
@@ -62,52 +63,55 @@ const service = new k8s.core.v1.Service(
   { provider: k8sProvider }
 );
 
-const ingress = new k8s.extensions.v1beta1.Ingress(
-  appName,
-  {
-    metadata: {
-      labels: appLabels,
-      annotations: {
-        "kubernetes.io/ingress.class": "traefik",
-        "ingress.kubernetes.io/force-hsts": "true",
-        "ingress.kubernetes.io/hsts-max-age": "315360000",
-        "ingress.kubernetes.io/hsts-include-subdomains": "true",
-        "ingress.kubernetes.io/hsts-preload": "true",
-        "ingress.kubernetes.io/browser-xss-filter": "true",
-        "ingress.kubernetes.io/content-type-nosniff": "true",
-        "ingress.kubernetes.io/custom-frame-options-value": "SAMEORIGIN",
-        "ingress.kubernetes.io/referrer-policy": "no-referrer-when-downgrade",
-        "ingress.kubernetes.io/content-security-policy":
-          "upgrade-insecure-requests",
-        "ingress.kubernetes.io/custom-response-headers":
-          "Feature-Policy: geolocation none; midi none; notifications none; push none; sync-xhr none; microphone none; camera none; magnetometer none; gyroscope none; speaker self; vibrate none; fullscreen self; payment none; ||",
+const ingressMiddleware = new k8s.apiextensions.CustomResource(appName, {
+  apiVersion: "traefik.containo.us/v1alpha1",
+  kind: "Middleware",
+  metadata: {
+    labels: appLabels,
+  },
+  spec: {
+    headers: {
+      forceSTSHeader: true,
+      stsSeconds: 31536000,
+      stsIncludeSubdomains: true,
+      stsPreload: true,
+      referrerPolicy: "no-referrer-when-downgrade",
+      contentTypeNosniff: true,
+      contentSecurityPolicy: "upgrade-insecure-requests",
+      browserXssFilter: true,
+      customFrameOptionsValue: "SAMEORIGIN",
+      customResponseHeaders: {
+        "Feature-Policy":
+          "geolocation none; midi none; notifications none; push none; sync-xhr none; microphone none; camera none; magnetometer none; gyroscope none; speaker self; vibrate none; fullscreen self; payment none;",
       },
     },
-    spec: {
-      rules: [
-        {
-          host: "www.aaronbatilo.dev",
-          http: {
-            paths: [
-              {
-                path: "/resume",
-                backend: {
-                  serviceName: service.metadata.name,
-                  servicePort: service.spec.ports[0].port,
-                },
-              },
-              {
-                path: "/resume.pdf",
-                backend: {
-                  serviceName: service.metadata.name,
-                  servicePort: service.spec.ports[0].port,
-                },
-              },
-            ],
-          },
-        },
-      ],
-    },
   },
-  { provider: k8sProvider }
-);
+});
+
+const ingressRoute = new k8s.apiextensions.CustomResource(appName, {
+  apiVersion: "traefik.containo.us/v1alpha1",
+  kind: "IngressRoute",
+  metadata: {
+    labels: appLabels,
+  },
+  spec: {
+    entryPoints: ["websecure"],
+    routes: [
+      {
+        match: "Host(`www.aaronbatilo.dev`) && Path(`/resume`, `/resume.pdf`)",
+        kind: "Rule",
+        middlewares: [
+          {
+            name: ingressMiddleware.metadata.name,
+          },
+        ],
+        services: [
+          {
+            name: service.metadata.name,
+            port: service.spec.ports[0].port,
+          },
+        ],
+      },
+    ],
+  },
+});
