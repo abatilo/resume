dac287dc60143d75760aa25c8eeb733ad0a44304
Aaron <AaronBatilo@gmail.com>
2021-04-17 17:19:52 -0600

    Switch to traefik

--- index.ts
+++ index.ts
@@ -49,64 +49,51 @@ const pdb = new k8s.policy.v1beta1.PodDisruptionBudget(
   { provider: k8sProvider }
 );
 
-const gateway = new k8s.apiextensions.CustomResource(
+const ingressMiddleware = new k8s.apiextensions.CustomResource(
   appName,
   {
-    apiVersion: "networking.istio.io/v1alpha3",
-    kind: "Gateway",
-    metadata: {
-      namespace: deployment.metadata.namespace,
-    },
+    apiVersion: "traefik.containo.us/v1alpha1",
+    kind: "Middleware",
+    metadata: { namespace: deployment.metadata.namespace },
     spec: {
-      selector: {
-        istio: "ingressgateway",
-      },
-      servers: [
-        {
-          port: {
-            number: 443,
-            name: "https",
-            protocol: "HTTP",
-          },
-          hosts: ["www.aaronbatilo.dev"],
+      headers: {
+        forceSTSHeader: true,
+        stsSeconds: 31536000,
+        stsIncludeSubdomains: true,
+        stsPreload: true,
+        referrerPolicy: "no-referrer-when-downgrade",
+        contentTypeNosniff: true,
+        contentSecurityPolicy: "upgrade-insecure-requests",
+        browserXssFilter: true,
+        customFrameOptionsValue: "SAMEORIGIN",
+        customResponseHeaders: {
+          "Feature-Policy":
+            "geolocation none; midi none; notifications none; push none; sync-xhr none; microphone none; camera none; magnetometer none; gyroscope none; speaker self; vibrate none; fullscreen self; payment none;",
         },
-      ],
+      },
     },
   },
   { provider: k8sProvider }
 );
 
-const virtualService = new k8s.apiextensions.CustomResource(
+const ingressRoute = new k8s.apiextensions.CustomResource(
   appName,
   {
-    apiVersion: "networking.istio.io/v1alpha3",
-    kind: "VirtualService",
+    apiVersion: "traefik.containo.us/v1alpha1",
+    kind: "IngressRoute",
     metadata: {
       namespace: deployment.metadata.namespace,
     },
     spec: {
-      hosts: ["www.aaronbatilo.dev"],
-      gateways: [gateway.metadata.name],
-      http: [
+      entryPoints: ["websecure"],
+      routes: [
         {
-          match: [
-            {
-              uri: {
-                exact: "/resume",
-              },
-            },
-            {
-              uri: {
-                exact: "/resume.pdf",
-              },
-            },
-          ],
-          route: [
-            {
-              destination: {
-                host: service.metadata.name,
-              },
-            },
+          match:
+            "Host(`www.aaronbatilo.dev`) && (Path(`/resume`) || Path(`/resume.pdf`))",
+          kind: "Rule",
+          middlewares: [{ name: ingressMiddleware.metadata.name }],
+          services: [
+            { name: service.metadata.name, port: service.spec.ports[0].port },
           ],
         },
       ],
