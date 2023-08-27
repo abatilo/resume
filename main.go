5f0ea53e21a19cdef962a43379b8bdeec883145f
Aaron Batilo <AaronBatilo@gmail.com>
2023-08-27 09:55:17 -0600

    Bring back content-type header

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ var resume []byte
 func main() {
 	http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "public, max-age=604800")
-		// w.Header().Set("Content-Type", "application/pdf")
+		w.Header().Set("Content-Type", "application/pdf")
 		w.Write(resume)
 	}))
 }
