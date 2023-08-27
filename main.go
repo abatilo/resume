e9001f3bce378edc0b2ddb63fbb393a8fc6d41bd
Aaron Batilo <AaronBatilo@gmail.com>
2023-08-27 08:55:51 -0600

    Add cache header

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ var resume []byte
 
 func main() {
 	http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "public, max-age=172800")
+		w.Header().Set("Content-Type", "application/pdf")
 		w.Write(resume)
 	}))
 }
