623b36b6bb192da0dc872f310bfad53a8a9c7e8d
Aaron Batilo <AaronBatilo@gmail.com>
2023-08-27 09:57:01 -0600

    Add extra content headers

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "public, max-age=604800")
 		w.Header().Set("Content-Type", "application/pdf")
+		w.Header().Set("Content-Disposition", "inline; filename=\"resume.pdf\"")
+		w.Header().Set("Content-Transfer-Encoding", "binary")
+		w.Header().Set("Content-Length", string(len(resume)))
 		w.Write(resume)
 	}))
 }
